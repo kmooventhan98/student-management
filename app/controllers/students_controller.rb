b14d19945ec3ab05ff3fb0bc455b46d3834c3c52
Mooventhan <mooventhan@progton.com>
2023-10-25 23:01:22 +0530

    added create

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,24 @@
 class StudentsController < ApplicationController
+	def index
+		@students = Student.all.order(created_at: :desc)
+	end
+
+	def new
+		@student = Student.new
+	end
+
+	def create
+		@student = Student.new(student_params)
+		if @student.save
+			redirect_to students_path
+		else
+			render :new, status: :unprocessable_entity
+		end
+	end
+
+
+	private
+	def student_params
+		params.require(:student).permit(:first_name, :last_name, :email)
+	end
 end
